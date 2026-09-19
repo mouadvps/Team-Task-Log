158229af42f085dcc0fce9a41be40452ada4ba56
mouadvps <86118605+mouadvps@users.noreply.github.com>
2026-09-19 12:06:57 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,3 @@
-// Firebase web app settings for the vps-team-log project.
-// These values are not secrets: access is protected by sign-in and the Firestore rules.
 window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyDWahP2AQlhMP0jpSxVwt_bz3-ggF6MmE4",
   authDomain: "vps-team-log.firebaseapp.com",
@@ -8,4 +6,3 @@ window.FIREBASE_CONFIG = {
   messagingSenderId: "52425981569",
   appId: "1:52425981569:web:72a1185771776c3cfc5780"
 };
- 
